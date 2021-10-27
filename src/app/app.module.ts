13ee62b8dfca77fd9038e516c9223a5f4e1785da
Tommaso Randazzo <trandazzo1998@gmail.com>
2021-10-27 15:37:33 -0400

    init angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ClientComponent } from './client/client.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ClientComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
